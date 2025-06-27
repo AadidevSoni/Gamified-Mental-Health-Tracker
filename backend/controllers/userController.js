fbe4278eb76696346e289530f94e2db320ce8a11
Aadidev Soni <aadidevbahrain@gmail.com>
2025-06-27 13:17:36 +0300

    Token and cookie generation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,44 +1,40 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
-const createUser = asyncHandler(async(req,res) => {
-  const {username,email,password} = req.body;
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
 
-  if(!username || !email || !password) {
+  if (!username || !email || !password) {
+    res.status(400);
     throw new Error("Please fill all the fields!");
   }
 
-  const userExists = await User.findOne({email});
+  const userExists = await User.findOne({ email });
 
-  if(userExists) {
+  if (userExists) {
     res.status(400);
     throw new Error("User already exists! Try logging in!");
-    // or just return to stop further execution
-  } 
+  }
 
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password,salt);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-  const newUser = new User({username,email,password:hashedPassword});
+  const newUser = new User({ username, email, password: hashedPassword });
 
-  try {   
+  await newUser.save();
 
-    await newUser.save();
+  // Set token cookie
+  createToken(res, newUser._id);
 
-    res.status(201).json({
-      _id: newUser._id, 
-      username: newUser.username, 
-      email: newUser.email, 
-      isAdmin: newUser.isAdmin,
-      exp: newUser.exp,
-      level: newUser.level,
-    });
-
-  } catch (error) {
-    res.status(400);
-    throw new Error("Invalid user data")
-  }
+  res.status(201).json({
+    _id: newUser._id,
+    username: newUser.username,
+    email: newUser.email,
+    exp: newUser.exp,
+    level: newUser.level,
+  });
 });
 
-export {createUser};
+export { createUser };
