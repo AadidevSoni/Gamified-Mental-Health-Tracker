9e5dc00d719d67fe5be7c7c8559a5c4aedf43021
Aadidev Soni <aadidevbahrain@gmail.com>
2025-06-28 13:36:48 +0300

    home page design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
       <div className="content">
         <h1 className='title'>Welcome to <br /> <span className='canopy'><span className='calm'>Calm</span><span className='canopy-word'>Canopy</span></span></h1>
         <p className="subtitle">Track your mood, habits, and progress toward mental wellness.</p>
+        <img className="firest" src="'/pictures/forest3.jpg'" alt="" />
       </div>
     </div>
   );
